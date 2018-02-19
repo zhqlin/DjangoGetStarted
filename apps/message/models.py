7a93e583d8b7dd01f992647aaa1424e128ce2fbd
zhqlin <zhqlin@gmail.com>
2018-02-20 00:21:31 +0800

    留言板数据库orm映射成表完成。内容截止教程3-3结束。

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class UserMessage(models.Model):
+    name = models.CharField(max_length=20, verbose_name='用户名')
+    email = models.EmailField(verbose_name='邮箱')
+    address = models.CharField(max_length=100, verbose_name='联系地址')
+    message = models.CharField(max_length=500, verbose_name='留言信息')
+
+    class Meta:
+        verbose_name = '用户留言信息'
+        verbose_name_plural = verbose_name
